93e5df8422a086f07fba695041fc25f9bdac82e5
lexe1 <41.relay@gmail.com>
2022-12-28 20:25:47 +0200

    cv deploy

--- website/settings.py
+++ website/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-# from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SCRIMMAGE_SECRET_KEY')
-SECRET_KEY = 'eafdF@8u2h9fu@9h2f@f'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -139,5 +140,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-CSRF_TRUSTED_ORIGINS = ['https://*.fly.dev',
-                        'https://*.run.app', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.run.app', 'https://*.127.0.0.1']
